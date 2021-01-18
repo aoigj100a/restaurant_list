7bbc44f92c69182d035dfbdc31f60d156a4e0b98
aoigj100a <aoigj100@gmail.com>
2021-01-18 18:22:16 +0800

    views準備就緒

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// 1.載入 mongoose
+const mongoose = require('mongoose') 
+
+//1.載入bodyParser 2.使用bodyParser轉碼
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+
 //1.載入樣板引擎 2.啟動引擎 3.設定引擎
-const handlebars = require('express-handlebars')
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+const exphbs = require('express-handlebars')
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 //告訴expres靜態資源都放在public
 app.use(express.static('public'))
@@ -29,14 +36,9 @@ app.get('/show/:no', (req, res) => {
     //2.去抓json 使用篩選器篩選出要顯示的資料(function記得要傳回值！！！)
     //3.去show.handlebars 印出資料
 
-    // console.log(req.params.no)
-    // const restaurants = restaurantList.results.find(odj =>
-    //     odj.id.toString() === req.params.no
-    // )
     const restaurants = restaurantList.results.find(function (odj) {
         return odj.id.toString() === req.params.no
     })
-    // console.log(restaurants)
     res.render('show', { restaurants: restaurants })
 
 })
@@ -47,22 +49,14 @@ app.get('/search', (req, res) => {
     //2.取得網址列中 ? 後的內容用於篩選資料
     //3.優化使用者體驗
 
-    // console.log('req.query', req.query)
     const kw = req.query.keyword
 
     const restaurants = restaurantList.results.filter(odj => {
-        //    odj.name === kw
-        // console.log(typeof(odj))
-        // const title = odj.name.toLowerCase().includes(kw.toLowerCase())
-        // const type = odj.category.toLowerCase().includes(kw.toLowerCase())
         const allin = odj.name.concat(odj.category).concat(odj.name_en).concat(odj.location)
         const compar = allin.toLowerCase().includes(kw.toLowerCase())
         return compar
     })
-    // console.log(restaurants)
-
     res.render('index', { restaurants: restaurants, keyword: kw })
-
 })
 
 app.listen(port, () => {
